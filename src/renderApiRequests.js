41a3ec9f28a591bb35e4ca0fd4407c5aabf72cec
e.pikhterev <e.pikhterev@corp.vk.com>
2021-05-12 19:26:22 +0300

    api requests and scopes

---
+++ src/renderApiRequests.js
@@ -0,0 +1,30 @@
+import templateApiRequestsList from './pages/components/api-requests-list.pug';
+
+const renderApiRequests = (requestsList) => {
+  const methodListEl = document.querySelector('#api-requests-list');
+  requestsList.forEach((request) => {
+    request.is_api_request = true;
+    request.params = [
+      {
+        "name": "method",
+        "title": "название метода API",
+        "default": request.name,
+        "required": true
+      },
+      {
+        "name": "request_id",
+        "title": "Произвольная строка",
+        "default": "1234"
+      },
+      {
+        "name": "params",
+        "title": "параметры метода API (параметр `access_token` никому передавать нельзя)",
+        "type": "json",
+        "default": JSON.stringify(request.params)
+      }
+    ];
+  });
+  methodListEl.innerHTML = templateApiRequestsList({"apiRequests": requestsList});
+};
+
+export default renderApiRequests
