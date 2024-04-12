ba8a3001d2b59f04b57d3b35988443efdf1502cd
e.pikhterev <e.pikhterev@corp.vk.com>
2024-04-12 15:32:44 +0300

    tests vk-bridge

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import "regenerator-runtime/runtime.js";
 import './css/style.css'
 import vkDirectGameApp from './vkDirectGameApp';
-import bridge from "@vkontakte/vk-bridge";
 
 window.vkDirectGameApp = new vkDirectGameApp();
 window.vkDirectGameApp.init();
-window.bridge = bridge;
